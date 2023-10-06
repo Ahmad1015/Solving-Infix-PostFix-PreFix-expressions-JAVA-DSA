a0b882fdc5b00a78d17608d823af6de78681b837
Ahmad <mahmedraza90@gmail.com>
2023-10-06 22:40:59 +0500

    Adding Array Code

--- Main.java
+++ Main.java
@@ -1,3 +1,61 @@
 public class Main{
-    
+    public static void main(String[] args){
+
+    }
+}
+
+abstract class List{
+    abstract boolean push(int element);
+    abstract int pop();
+    abstract boolean isEmpty();
+    abstract boolean isFull();
+}
+class Stack extends List{
+    int top;
+    int N;                  // Total Size of Array;
+    int arr[];
+    public Stack(){
+        top=0;
+        N=5;
+        arr = new int[N];
+    }
+    public Stack(int size){
+        top = 0;
+        N = size;
+        arr = new int[N];
+    }
+    public boolean push(int element){
+        if(top==N){
+            System.out.println("Stack is Full- Cannot Push");
+            return false;
+        }
+        else{
+            arr[top]=element;
+            top++;
+            return true;
+        }
+    }
+    public int pop(){
+        int element_to_return = arr[top-1];
+        top--;
+        return element_to_return;
+    }
+    public boolean isEmpty(){
+        if(top == 0)
+            return true;
+        else
+            return false;
+    }
+    public boolean isFull(){
+            return top==N;
+    }
+    @Override
+    public String toString(){
+        String stringElement = "";
+        for(int i=0;i<top;i++){
+            stringElement = stringElement+" "+String.valueOf(arr[i]);
+        }
+        return stringElement+=" -> null";
+    }
+
 }
